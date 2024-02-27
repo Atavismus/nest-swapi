e3ee69fc62df035ff0117c0fb67225bcb6c1ad6a
Atavismus <soleinoirdsr@hotmail.com>
2024-02-27 10:26:06 +0100

    add search route

--- src/resources/resources.controller.ts
+++ src/resources/resources.controller.ts
@@ -2,21 +2,38 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ResourcesService } from './resources.service';
 import { Resources } from './interfaces/resources.interface';
 
-@Controller('api/get')
+@Controller('api')
 /**
- * Get an entry by resource and id or get all entries from a resource if no id parameter
- * @param {string} resource
- * @param {integer} id (optional)
- * @param {integer} page (optional)
+ * Get an entry by resource and id or get all entries from a resource if id parameter is omitted.
+ * @param {string} resource - The type of resource to search for. Must be a valid resource type.
+ * @param {integer} [id] The id of the entry to retrieve. Optional.
+ * @param {integer} [page] - The page number to retrieve. Optional.
+ * @return {Promise<Resource[]>} One or several entries of the queried resource.
  */
 export class ResourcesController {
   constructor(private resourcesService: ResourcesService) {}
-  @Get()
+  @Get('get')
   async find(
     @Query('resource') resource: string,
     @Query('id') id?: string,
     @Query('page') page?: string,
   ): Promise<Resources[]> {
     return this.resourcesService.find(resource, id, page);
+  }
+
+  /**
+   * Search for entries that match the given resource and search term.
+   * @param {string} resource - The type of resource to search for. Must be a valid resource type.
+   * @param {string} search - The search term to look for within the specified resource (mostly its name).
+   * @param {integer} [page] - The page number to retrieve. Optional.
+   * @return {Promise<Resource[]>} One or several entries of the queried resource matching the search term.
+   */
+  @Get('search')
+  async search(
+    @Query('resource') resource: string,
+    @Query('search') search: string,
+    @Query('page') page?: string,
+  ): Promise<Resources[]> {
+    return this.resourcesService.search(resource, search, page);
   }
 }
--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -17,7 +17,24 @@ export class ResourcesService {
       );
       return response.data;
     } catch (error) {
-      error.message = `Bad request: ${error.message}`;
+      error.message = `Bad request - /api/get failed: ${error.message}`;
+      throw error;
+    }
+  }
+
+  async search(
+    resource: string,
+    search: string,
+    page?: string,
+  ): Promise<Resources[]> {
+    page = page ? `&page=${page}` : '';
+    try {
+      const result = await axios.get(
+        `${process.env.API_URL}${resource}/?search=${search}${page}`,
+      );
+      return result.data;
+    } catch (error) {
+      error.message = `Bad request - /api/search failed: ${error.message}`;
       throw error;
     }
   }
